6d50cc8fb0262900c6fc91589295d5e1e30fcb31
Rony_Sorker <mdronysarkar33299@gmail.com>
2023-10-27 10:36:05 +0600

    javascript

---
+++ class-2.js
@@ -0,0 +1,20 @@
+// DATA types
+//string
+//anything is written inside two "" quotation or single '' quotation
+
+// number 
+// any number is number data type
+
+//boolean
+//any thing true or false is boolean
+
+document.write("");
+document.write("");
+document.write("");
+
+//single line comment
+
+/*
+multiple line comment
+*/
+
