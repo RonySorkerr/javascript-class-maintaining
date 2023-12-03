7fecd30be6840a4949bb90477a9bc805368d146b
Rony_Sorker <mdronysarkar33299@gmail.com>
2023-12-03 10:21:35 +0600

    q

---
+++ making a calculator/onclick/script.js
@@ -0,0 +1,10 @@
+function myMessage(){
+    var message = document.querySelector("#para");
+    message.innerHTML = "You have pressed the first button";
+}
+
+function taped__message(){
+    var message_1 = document.querySelector("#para");
+    message_1.innerHTML = "You have clicked the second button";
+}
+
