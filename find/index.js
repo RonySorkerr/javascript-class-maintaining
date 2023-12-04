d03e9394a5be22c03510584937e4a068de44c4d4
Rony_Sorker <mdronysarkar33299@gmail.com>
2023-12-04 09:35:33 +0600

    e

---
+++ find/index.js
@@ -0,0 +1,7 @@
+var link = document.getElementsByTagName("a")[0];
+link.style.color = "red";
+link.style.textDecoration = "none";
+link.style.fontSize = "1.5rem";
+link.style.backgroundColor = "wheat";
+link.style.padding = "10px"
+link.style.fontStyle = ""
