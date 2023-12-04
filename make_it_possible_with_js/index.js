9bb9dddc9b445f6c66a56ea7f76e77a810dc3ffe
Rony_Sorker <mdronysarkar33299@gmail.com>
2023-12-04 12:33:39 +0600

    w

---
+++ make_it_possible_with_js/index.js
@@ -0,0 +1,14 @@
+// // const btn_1 = document.querySelector(".btn_1");
+
+// // btn_1.addEventListener('click',()=>{
+// //     btn_1.classList.add('colorful')
+// // });
+
+// const btnELList = document.querySelectorAll('.btn');
+
+// btnELList.forEach(btnEL => {
+//     btnEL.addEventListener('click', () =>{
+//         document.querySelector('colorful')?.classList.remove('colorful');
+//         btnEL.classList.add('colorful');
+//     });
+// });
